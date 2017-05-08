e4f9b0a7d2595bedc821635a329cc332d5062067
ThePhisics101 <thephisics101@gmail.com>
2017-05-09 00:36:57 +0300

    Initial commit

--- script.js
+++ script.js
@@ -1,36 +1,64 @@
-Array.prototype.iop2D = function (input) {
-    function iop (arr, input) {
-        for (let i = 0; i < arr.length; i++) {
-            if (arr[i][Object.keys(input)[0]] === Object.values(input)[0]) return i;
-        }
-        return -1;
-    }
-
-    for (let i = 0; i < this.length; i++) {
-        if (!this.hasOwnProperty(i)) continue;
-        if (!Array.isArray(this[i])) continue;
-        if (iop(this[i], input) !== -1) return [parseInt(i), this[i].iop(input)];
-    }
-    return -1;
-};
-
-(a => {
-    if (![1, 2, 3].includes(a)) return;
-
+(() => {
     let body = (() => {
         let body = document.createElement("div");
         body.classList.add("body");
         document.body.appendChild(body);
         return body;
-    })(), grid = [], s = a * 8;
+    })(), grid = [];
 
-    for (let x = 0; x < s; x++) {
-        grid.push([]);
-        //TODO: make a horizontal div
-        for (let y = 0; y < s; y++) {
-            let elem = document.createElement("div");
-            grid[x].push(new Tile(x, y, elem));
-            //TODO: do things to `elem`
+    // difficulty selector
+    (function chooseDiff() {
+        let selector = [
+            {
+                text: "Easy (8x8, 10 mines)",
+                size: 8,
+                mines: 10
+            },
+            {
+                text: "Medium (16x16, 40 mines)",
+                size: 16,
+                mines: 40
+            },
+            {
+                text: "Hard (24x24, 99 mines)",
+                size: 24,
+                mines: 99
+            }
+        ], picker = document.createElement("div");
+        body.appendChild(picker);
+        for (let i of selector) {
+            let div = document.createElement("div");
+            picker.appendChild(div);
+            div.classList.add("diff");
+            div.appendChild(document.createTextNode(i.text));
+            div.addEventListener("click", m => {
+                m.preventDefault();
+                if (document.getElementById("gridBody"))
+                    body.removeChild(document.getElementById("gridBody"));
+                createGrid(i.size, i.mines);
+            });
+        }
+    })();
+
+    function createGrid(size, mineCount) {
+        let gridBody = document.createElement("div");
+        gridBody.id = "gridBody";
+        body.appendChild(gridBody);
+        for (let y = 0; y < size; y++) {
+            grid.push([]);
+            let row = document.createElement("div");
+            row.classList.add("row");
+            gridBody.appendChild(row);
+            //TODO: make a horizontal div
+            for (let x = 0; x < size; x++) {
+                let tile = document.createElement("div");
+                tile.classList.add("tile");
+                tile.style.width = `${640 / size}px`;
+                tile.style.height = `${640 / size}px`;
+                row.appendChild(tile);
+                grid[y].push(new Tile(x, y, tile));
+                //TODO: do things to `elem`
+            }
         }
     }
 
@@ -52,5 +80,21 @@ Array.prototype.iop2D = function (input) {
             //TODO: implement flags
         }
     }
-})(parseInt(location.hash.substr(1)));
+
+    function iop2D (array, input) { // index of object in a 2D array
+        function iop (arr, input) { // index of object in an array
+            for (let i = 0; i < arr.length; i++) {
+                if (arr[i][Object.keys(input)[0]] === Object.values(input)[0])
+                    return i;
+            }
+            return -1;
+        }
+
+        for (let i = 0; i < array.length; i++) {
+            if (!Array.isArray(array[i])) continue;
+            if (iop(array[i], input) !== -1) return [parseInt(i), array[i].iop(input)];
+        }
+        return -1;
+    }
+})();
 //.target
